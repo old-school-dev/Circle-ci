06621e539da8241757d50dbca95470276661e4dd
OldSchool Dev <fam.b.boy2@gmail.com>
2022-03-17 21:03:21 +0700

    change name test to email

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -24,6 +24,6 @@ class ExampleTest extends TestCase
             'email' => "test@mail.com",
             'password' => "testpassword"
         ]);
-        $this->assertEquals('Test User', $user->name);
+        $this->assertEquals('test@mail.com', $user->email);
     }
 }
