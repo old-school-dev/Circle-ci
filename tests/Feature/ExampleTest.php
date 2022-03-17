a3e2c1192a64d957218a91127e624ed3224ee98d
OldSchool Dev <fam.b.boy2@gmail.com>
2022-03-17 23:46:14 +0700

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,23 +18,4 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
-    public function testUserCreationEndpointTest()
-    {
-        $name = $this->faker->name();
-        $email = $this->faker->email();
-        $password = "mypassword";
-
-        $response = $this->postJson('/api/createuser', [
-            'name' => $name, 
-            'email' => $email,
-            'password' => $password,
-            'password_confirmation' => $password
-        ]); 
-
-        $response
-            ->assertStatus(201)
-            ->assertExactJson([
-                'message' => "Successfully created user!",
-            ]);
-    }
 }
